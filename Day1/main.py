def6108a9f7d7016cd6e0637ebea8b82e3d05da2
P3rdigas <pr.perdigao@campus.fct.unl.pt>
2023-12-11 18:59:22 +0000

    Day 1 Part 2

--- Day1/main.py
+++ Day1/main.py
@@ -1,4 +1,5 @@
 def main():
+    # Part one
     sum = 0
     first = None
     last = None
@@ -26,7 +27,64 @@ def main():
         first = None
         last = None      
 
-    print(sum)
+    print("Part one sum: ", sum)
+
+    # Part two
+    sum = 0
+    first = None
+    last = None
+
+    numbersDic = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+
+    f = open("input2.txt", "r")
+    lines = f.readlines()
+
+    for line in lines:
+        new_str = ""
+        for c in line:
+            if c.isdigit():
+                if first is None :
+                    first = c
+                else:
+                    last = c
+                new_str = ""
+            else:
+                new_str += c
+            
+                numbers = numbersDic.keys()
+
+                for number in numbers:
+                    if number in new_str:
+                        if first is None :
+                            first = numbersDic[number]
+                        else:
+                            last = numbersDic[number]
+                        new_str = ""
+                        new_str += c
+
+        number = 0
+
+        if first is not None and last is not None:
+            number = str(first) + str(last)
+            sum += int(number)
+        elif first is not None :
+            number = str(first) + str(first)
+            sum += int(number)
+
+        first = None
+        last = None
+
+    print("Part two sum: ", sum)
 
 if __name__ == "__main__":
     main()
